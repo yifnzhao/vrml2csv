a3d39027143ece53f425db0e6b715ca08f960c9c
Yifan Zhao <44952562+yifnzhao@users.noreply.github.com>
2019-08-07 12:29:18 -0400

    Update normalcalc.py

--- normalcalc.py
+++ normalcalc.py
@@ -7,26 +7,17 @@ Created on Mon Aug  5 11:43:22 2019
 """
 
 #normalcalc
-
-from numpy import subtract
-import numpy as np
-import pandas as pd
+from numpy import genfromtxt,savetxt, subtract,cross
 
 def normal(v0, v1, v2, ccw = True):
-    #edge v0->v1, edge v1->v2
-    if ccw == True:
-        return np.cross(subtract(v1,v0),subtract(v2,v1)).astype('float16')
+    if ccw == True: 
+        return cross(subtract(v1,v0),subtract(v2,v1)).astype(float)
     elif ccw == False:
-        return np.cross(subtract(v1,v0),subtract(v2,v0)).astype('float16')
-
-
-def df_as_list(csv_dir):
-    df=pd.read_csv(csv_dir, header = None, index_col=[0])
-    return df.values.tolist()
+        return cross(subtract(v1,v0),subtract(v2,v0)).astype(float)
 
 def getFaceNormal(root, nodeCount, CCW = True):
-    coord = df_as_list(root+'_coord_'+str(nodeCount)+'.csv')
-    index = df_as_list(root+'_index_'+str(nodeCount)+'.csv')
+    coord = genfromtxt(root+'_coord_'+str(nodeCount)+'.csv', delimiter=',')
+    index = genfromtxt(root+'_index_'+str(nodeCount)+'.csv', delimiter=',')
     faceNormal = []
     for face in index:
         v0 = coord[int(face[0])]
@@ -34,5 +25,5 @@ def getFaceNormal(root, nodeCount, CCW = True):
         v2 = coord[int(face[2])]
         n = normal(v0, v1, v2, ccw=CCW)
         faceNormal.append(n)
-    np.savetxt(root+'_faceNormal_'+str(nodeCount)+'.csv',
-               faceNormal,delimiter=",")
+    savetxt(root+'_faceNormal_'+str(nodeCount)+'.csv',
+               faceNormal,fmt = '%.6f', delimiter=',')
